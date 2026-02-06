4cf2f5df34918a923f2a0750e1c526b54e2bea21
Logan Marchione <logan@loganmarchione.com>
2026-02-05 19:52:20 -0500

    Add B2 bucket for PBS

--- production/b2.tf
+++ production/b2.tf
@@ -13,3 +13,19 @@ resource "b2_bucket" "marchione-bucket02" {
     file_name_prefix              = ""
   }
 }
+
+resource "b2_bucket" "marchione-homelab-pbs" {
+  bucket_name = "marchione-homelab-pbs"
+  bucket_type = "allPrivate"
+
+  default_server_side_encryption {
+    algorithm = null
+    mode      = "none"
+  }
+
+  lifecycle_rules {
+    days_from_hiding_to_deleting  = 1
+    days_from_uploading_to_hiding = 0
+    file_name_prefix              = ""
+  }
+}
