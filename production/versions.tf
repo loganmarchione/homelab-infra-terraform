31637db53a85dd71000b968e525dcc6a7412d899
Logan Marchione <logan@loganmarchione.com>
2026-07-25 23:04:43 -0400

    Bump Cloudflare to 5.21.1

--- production/versions.tf
+++ production/versions.tf
@@ -11,7 +11,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.16.0"
+      version = "~> 5.21.1"
     }
     digitalocean = {
       source  = "digitalocean/digitalocean"
